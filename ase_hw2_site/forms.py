ac5c7865214cc45febda058b40c56377dca2c00a
YixuanAshleyGuo <yg2430@cumc.columbia.edu>
2016-10-03 11:19:58 -0400

    data transfer from front-end to backend
    using form to pack the data and store to database in Mysql

--- ase_hw2_site/forms.py
+++ ase_hw2_site/forms.py
@@ -1,3 +1,15 @@
 from django import forms
+import datetime
 from django.db import connection
 
+class EventsForm(forms.Form):
+    tag = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=255)
+    time = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M'], widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local'}),initial=datetime.date.today)
+    #expiration = forms.DateTimeField(widget=forms.widgets.DateTimeInput(input_formats=["%d %b %Y %H:%M:%S %Z"]))
+    expiration = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M'],widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local'}),initial=datetime.date.today)
+    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=1024)
+    location = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=255)
+
+#    class Meta:
+#        managed = False
+#        db_table = 'Events'
--- ase_hw2_site/views.py
+++ ase_hw2_site/views.py
@@ -1,16 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db import connection
+from django.contrib import messages
+
+from .forms import EventsForm
 
 from django.contrib.auth.decorators import login_required
 
-def index(request):
+def index(request):    
+    #if this is a POST request
+    if request.method == 'POST':
+        form = EventsForm(request.POST)
+        #check for validation
+        if form.is_valid():
+            #form.save()
+            data = form.cleaned_data
+            with connection.cursor() as cursors:
+                cursors.execute("INSERT INTO events (tag,time,expiration,description, location) VALUES (%s,%s,%s,%s,%s);",
+                                [data['tag'],data['time'],data['expiration'],data['description'],data['location']])
+            return HttpResponse("Thank you for submiting")
+        else:
+            HttpResponse("Not successful")
+    else:
+        form = EventsForm()
+            
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM events")
+        cursor.execute("SELECT * FROM events;")
         events = dictfetchall(cursor)
+    eventsform = EventsForm();
+    
     
     return render(request, 'ase_hw2_site/index.html',{
         'events': events,
+        'events_form': eventsform,
+        'form': form,
     })
 
 def dictfetchall(cursor):
